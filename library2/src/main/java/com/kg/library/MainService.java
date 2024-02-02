50f5609e07a180884ce2db4adacc3da933f00a65
bjw <dtktop7@naver.com>
2024-02-02 13:41:52 +0900

    수정

--- library2/src/main/java/com/kg/library/MainService.java
+++ library2/src/main/java/com/kg/library/MainService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -17,8 +18,29 @@ import org.springframework.web.client.RestTemplate;
 
 import com.kg.library.Introduction.BookDTO;
 
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+
 @Service
 public class MainService {
+
+	// s3에 이미지 추가
+	private String bucketName = "kglibrary"; // S3 버킷 이름
+	private String s3FilePath = "static/img/"; // S3에 업로드할 경로
+
+	@Autowired
+	private S3Client s3Client; // AWS S3 클라이언트 주입
+
+	private String getS3ObjectUri(String s3Key) {
+		if (s3Key == null || s3Key.trim().isEmpty()) {
+			// s3Key가 null 또는 빈 문자열인 경우에 대한 예외 처리
+			return ""; // 또는 다른 기본값 설정
+		}
+
+		GetUrlRequest getUrlRequest = GetUrlRequest.builder().bucket(bucketName).key(s3Key).build();
+
+		return s3Client.utilities().getUrl(getUrlRequest).toExternalForm();
+	}
 
 	private static final Logger logger = LoggerFactory.getLogger(MainService.class);
 
@@ -29,24 +51,28 @@ public class MainService {
 
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON);
-			
+
 			ResponseEntity<ArrayList<BookDTO>> responseEntity = restTemplate.exchange(
-			        "https://www.bowfun.link/book/hit_book", HttpMethod.GET, new HttpEntity<>(headers), 
-			        new ParameterizedTypeReference<ArrayList<BookDTO>>() {
-			        });
+					"https://www.bowfun.link/book/hit_book", HttpMethod.GET, new HttpEntity<>(headers),
+					new ParameterizedTypeReference<ArrayList<BookDTO>>() {
+					});
 
 			ArrayList<BookDTO> hitbooks = responseEntity.getBody();
-			System.out.println("hit_book 요청 보냄");
+			ArrayList<String> imageUrls = new ArrayList<>();
+
 			if (hitbooks != null) {
 				for (BookDTO b : hitbooks) {
 					if (b.getImage() == null || b.getImage().trim().isEmpty()) {
 						b.setImage("20240109150111-40641325628.20230718121618.jpg");
 						continue;
 					}
+					String imageUrl = getS3ObjectUri(b.getImage());
+					imageUrls.add(imageUrl);
 				}
 			}
 			// 모델에 데이터 추가
 			model.addAttribute("hitbooks", hitbooks);
+			model.addAttribute("imageUrls", imageUrls);
 
 		} catch (HttpClientErrorException.NotFound notFoundException) {
 			// 404 Not Found 에러 처리
@@ -62,19 +88,20 @@ public class MainService {
 	public void new_book(Model model) {
 
 		try {
-			
+
 			RestTemplate restTemplate = new RestTemplate();
 
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON);
-			
+
 			ResponseEntity<ArrayList<BookDTO>> responseEntity = restTemplate.exchange(
-					"https://www.bowfun.link/book/new_Book", HttpMethod.GET, new HttpEntity<>(headers), 
+					"https://www.bowfun.link/book/new_Book", HttpMethod.GET, new HttpEntity<>(headers),
 					new ParameterizedTypeReference<ArrayList<BookDTO>>() {
 					});
 
 			ArrayList<BookDTO> newBooks = responseEntity.getBody();
-			System.out.println("newBooks 요청 보냄");
+			ArrayList<String> imageUrls = new ArrayList<>();
+			
 			if (newBooks != null) {
 				for (BookDTO b : newBooks) {
 					System.out.print("\u001B[31m");
@@ -91,10 +118,13 @@ public class MainService {
 					{
 						continue;
 					}
+					String imageUrl = getS3ObjectUri(b.getImage());
+					imageUrls.add(imageUrl);
 				}
 			}
 			// 모델에 데이터 추가
 			model.addAttribute("newBooks", newBooks);
+			model.addAttribute("imageUrls", imageUrls);
 
 		} catch (HttpClientErrorException.NotFound notFoundException) {
 			// 404 Not Found 에러 처리
